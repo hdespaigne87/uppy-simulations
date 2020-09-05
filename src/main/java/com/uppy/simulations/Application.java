33d2208ba1dd8cae20e8be19f9d1f1e8405c957b
Husseyn Despaigne Reyes <husseyn.despaignereyes@gmail.com>
2020-09-05 12:38:28 -0400

    Update project package

--- src/main/java/com/uppy/simulations/Application.java
+++ src/main/java/com/uppy/simulations/Application.java
@@ -3,11 +3,13 @@ package com.uppy.simulations;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     @Bean
     public RestTemplate restTemplate() {
@@ -16,5 +18,11 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder
+                                                         builder) {
+        return builder.sources(Application.class);
     }
 }
